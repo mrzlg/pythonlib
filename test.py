05b2e066895ca651deb7cf0e56d12d9e3417fc7f
mrzlg <xsanbbshryds@gmail.com>
2016-08-01 16:07:16 +0800

    2

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ db = MyDB( {'host':"localhost",'port':3306,'user':"root",'passwd':"123123",'db':
 sql = "SELECT * FROM `log`"
 db.query(sql)
 # 获取结果列表
-result = db.fetchAllRows( );
+result = db.fetchAllRows( )
 
 # 相当于php里面的var_dump
 print result
-db.close()
+# db.close()
