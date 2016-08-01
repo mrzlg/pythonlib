0dec2a1d3f8ec48dd31cfb71ea33eab1d72f09d6
mrzlg <xsanbbshryds@gmail.com>
2016-08-01 16:21:02 +0800

    22

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ from db.MyDB import MyDB
 
 db = MyDB( {'host':"localhost",'port':3306,'user':"root",'passwd':"123123",'db':'test','charset':'utf8'})
 # db.connect()
+# INSERT INTO `landmap` VALUES (1,'',1,0,1,1,0,0,1,2,'','');
+sql = "INSERT INTO `log` VALUES (13234,'2','234234','ttt',2,'zxsds dfg sdfg sdf gsdfg sdfgs ')"
+db.insert(sql)
 sql = "SELECT * FROM `log`"
 db.query(sql)
 # 获取结果列表
