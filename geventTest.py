a132dd83f7308fc3cd27e57cf1623fe2fed13cf2
xsanbbshryds@gmail.com <align9527>
2016-08-25 23:50:49 +0800

    11

---
+++ geventTest.py
@@ -0,0 +1,37 @@
+import gevent.monkey
+
+import urllib2
+import simplejson as json
+
+import gevent
+
+gevent.monkey.patch_socket()
+
+
+def fetch(pid):
+    response = urllib2.urlopen('http://json-time.appspot.com/time.json')
+    result = response.read()
+    json_result = json.loads(result)
+    datetime = json_result['datetime']
+
+    print('Process %s: %s' % (pid, datetime))
+    return json_result['datetime']
+
+
+def synchronous():
+    for i in range(1, 10):
+        fetch(i)
+
+
+def asynchronous():
+    threads = []
+    for i in range(1, 10):
+        threads.append(gevent.spawn(fetch, i))
+    gevent.joinall(threads)
+
+
+print('Synchronous:')
+synchronous()
+
+print('Asynchronous:')
+asynchronous()
